921706e110ceccd3e0a9ef3425a22e5ed3f58ee5
huda <you@example.com>
2020-04-05 14:14:07 +0300

    create postes controller& Add show

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 devise_for :users
+ get 'postes/:id' , to: 'postes#show'
  get 'pages/home'
  get 'pages/about'
  get 'about',to:'pages#about'
--- blog/test/controllers/postes_controller_test.rb
+++ blog/test/controllers/postes_controller_test.rb
@@ -1,14 +1,7 @@
 require 'test_helper'
 
 class PostesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get postes_home_url
-    assert_response :success
-  end
-
-  test "should get about" do
-    get postes_about_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
