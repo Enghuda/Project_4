63e1df35c39f4af991beaa1794899f877aeb24e4
huda <you@example.com>
2020-04-05 14:42:51 +0300

    Add index

--- blog/app/controllers/postes_controller.rb
+++ blog/app/controllers/postes_controller.rb
@@ -2,4 +2,7 @@ class PostesController < ApplicationController
     def show
      @poste = Poste.find(params[:id])   
     end
+    def index
+    @postes = Poste.all
+    end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 devise_for :users
- get 'postes/:id' , to: 'postes#show'
+ get 'postes/:id' , to: 'postes#show' , as: 'poste'
+ get 'postes' , to: 'postes#index'
+
  get 'pages/home'
  get 'pages/about'
  get 'about',to:'pages#about'
