f9baf29347539b95fd114342ffd6af04b7a455eb
huda <you@example.com>
2020-04-05 19:30:39 +0300

    create edit&update&destroy

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 devise_for :users
+get 'postes/new' , to: 'postes#new'
  get 'postes/:id' , to: 'postes#show' , as: 'poste'
+ post 'postes' , to: 'postes#create'
  get 'postes' , to: 'postes#index'
+ get 'postes/:id/edit' , to: 'postes#edit', as:'poste_edit' 
+ delete 'postes/:id', to: 'postes#destroy' , as: 'postes_delete'
+ patch  'postes/:id', to: 'postes#update'
+
 
  get 'pages/home'
  get 'pages/about'
