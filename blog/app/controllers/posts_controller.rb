4b06e45886a0b9905dc4147edb10aeb32b09ec1c
huda <you@example.com>
2020-04-06 20:55:26 +0300

    not working

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only:[:new , :show]
+    before_action :find_post , only: [:show, :edit, :update, :destroy]
+
+    def show
+    end
+
+    def index
+    @posts = Post.all
+    end
+
+    def new
+    @post = Post.new
+    end
+    def create
+    @post = Post.new(post_params)
+    if @post.save
+    redirect_to @post
+    else 
+        render 'new'
+    end
+end
+def edit
+end
+def update
+    if @post.update(post_params)
+        redirect_to post_path(@post)
+    else
+        render 'edit'
+    end
+end
+def destroy
+    @post.destroy
+    redirect_to root_path
+end
+
+    private
+    def post_params
+        params.required(:post).permit(:name , :description , :image)       
+    end
+    def find_post
+        @post = Post.find(params[:id])   
+    end
+end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 devise_for :users
-get 'postes/new' , to: 'postes#new'
- get 'postes/:id' , to: 'postes#show' , as: 'poste'
- post 'postes' , to: 'postes#create'
- get 'postes' , to: 'postes#index'
- get 'postes/:id/edit' , to: 'postes#edit', as:'poste_edit' 
- delete 'postes/:id', to: 'postes#destroy' , as: 'postes_delete'
- patch  'postes/:id', to: 'postes#update'
+
+resources :comments
+get 'posts/new' , to: 'posts#new'
+ get 'posts/:id' , to: 'posts#show' , as: 'post'
+ post 'posts' , to: 'posts#create'
+ get 'posts' , to: 'posts#index'
+ get 'posts/:id/edit' , to: 'posts#edit', as:'post_edit' 
+ delete 'posts/:id', to: 'posts#destroy' , as: 'posts_delete'
+ patch  'posts/:id', to: 'posts#update'
 
 
  get 'pages/home'
