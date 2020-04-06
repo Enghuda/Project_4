6e4a7bdd4c2aa3b4a3bee2558ad8de20ddbd2c1a
huda <you@example.com>
2020-04-06 21:00:40 +0300

    restart all

--- blog/app/models/poste.rb
+++
@@ -1,2 +0,0 @@
-class Poste < ApplicationRecord
-end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_04_05_091551) do
 
-  create_table "postes", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
