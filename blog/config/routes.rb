17e6d0e608c0ca07d18b0f40927b7b21c83bf820
huda <you@example.com>
2020-04-07 09:03:04 +0300

    Add image in index

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -10,11 +10,6 @@ get 'posts/new' , to: 'posts#new'
  get 'posts/:id/edit' , to: 'posts#edit' 
  delete 'posts/:id', to: 'posts#destroy' 
  patch  'posts/:id', to: 'posts#update'
-
-
- 
-
-
  get 'pages/home'
  get 'pages/about'
  get 'about',to:'pages#about'
