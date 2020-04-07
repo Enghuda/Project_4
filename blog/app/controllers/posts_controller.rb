0d8100902f718513f4b6e9a1452260040eea27fc
huda <you@example.com>
2020-04-08 02:45:28 +0300

    delede comm for user

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only:[:show, :new,:edit, :destroy]
+  before_action :set_post, except: [:index, :new, :create]
+  before_action :authenticate_user!, only:[:new , :show]
   # GET /posts
   # GET /posts.json
   def index
