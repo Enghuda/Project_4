b5ba073e39f014e8277f12231da303909e2b6c5b
huda <you@example.com>
2020-04-07 09:41:40 +0300

    delete

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -10,5 +10,7 @@ class CommentsController < ApplicationController
             private
             def comment_params
               params.require(:comment).permit(:body)
-            end
 end
+end
+
+
--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
     has_one_attached :image
-    has_many :comments
+    has_many :comments, dependent: :destroy
 end
